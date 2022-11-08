5e45629681c1d9e097a5b0fc5b7d038fab08d0f5
LizbethRsAz <20301061550100@cetis155.edu.mx>
2022-11-08 08:27:24 -0600

    si

---
+++ inicio.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import pymysql
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+@app.route("/informacion")
+def informacion():
+    return render_template("informacion.html")
+
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
